54be37a0f314ccdb4d1043eb7a3156e9a43a963c
bashunmcgee <bashunmcgee93@gmail.com>
2024-04-17 05:05:24 -0700

    Good Stopping point here
    My Web app provides basic presentation where my back end is able to utilize django for urls I have a small DB SQLite for demonstration with info on hand

---
+++ vidly/movies/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Movie
+# Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    movie = Movie.objects.get(pk=movie_id)
+    return render(request, 'movies/detail.html', {'movie': movie})
